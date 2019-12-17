803be704a569903c59fc0debc17b5b47b5a8e7ee
pang <pang@zhaw.ch>
2019-12-17 16:46:10 +0100

    made insights optional

--- insights.py
+++ insights.py
@@ -1,9 +1,11 @@
 import json
 import syncer
+import os
 
 
 def report(path, tool, node):
-    with open('{}/insights.json'.format(path), 'r') as input:
-        insights = json.load(input)
-        print(insights)
-        syncer.write('insight/{}/{}'.format(tool, node), json.dumps(insights))
+    if os.path.isfile('{}/insights.json'.format(path))
+        with open('{}/insights.json'.format(path), 'r') as input:
+            insights = json.load(input)
+            print(insights)
+            syncer.write('insight/{}/{}'.format(tool, node), json.dumps(insights))
