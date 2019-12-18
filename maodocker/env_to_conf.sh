b6c3a4866d23102772718d53c0935c49cb465097
pang <pang@zhaw.ch>
2019-12-18 16:24:49 +0100

    better config for compose

---
+++ maodocker/env_to_conf.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+echo "[WORKING_ENVIRONMENT]\n"\
+"importdir = $importdir\n"\
+"auditdir = $auditdir\n"\
+"user = $workuser\n\n"\
+"[ETCD]\n"\
+"host = $etcdhost\n"\
+"port = $port\n\n"\
+"[POSTGRES]\n"\
+"user = $dbuser\n"\
+"password = $password\n"\
+"db = $db\n"\
+"host = $dbhost\n\n"\
+"[DATA_REPOS]" >> config.ini
