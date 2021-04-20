e606c91488a1b0a1ce873025d95e3eed4fe5a667
Daniel Hass <hassdan1@students.zhaw.ch>
2021-04-20 12:43:13 +0000

    moved pipeline store to psql

--- syncer.py
+++ syncer.py
@@ -1,18 +1,13 @@
-import git
-import datetime
 import configparser
-import requests
 import json
 import schedule
-import shutil
-import base64
-import glob
 from datetime import datetime
-import logging
 import os
 import subprocess
 from etcd_client import write, get
-#import collect
+import sqlalchemy
+from sqlalchemy import Table, Column, String, select, cast
+from sqlalchemy.dialects.postgresql import JSONB
 
 #logging.basicConfig(level=logging.DEBUG)
 config = configparser.ConfigParser()
@@ -25,10 +20,26 @@ user = config['WORKING_ENVIRONMENT']['USER']
 if importdir.endswith('/'):
     importdir = importdir[:-1]
 
+### PostgreSQL handling for pipeline store
+psql_user = config['POSTGRES']['user']
+psql_password = config['POSTGRES']['password']
+psql_db = config['POSTGRES']['db']
+psql_host = config['POSTGRES']['host']
+psql_port = 5432
+# connect to database
+psql_url = f'postgresql://{psql_user}:{psql_password}@{psql_host}:{psql_port}/{psql_db}'
+psql_con = sqlalchemy.create_engine(psql_url, client_encoding='utf8')
+psql_meta = sqlalchemy.MetaData(bind=psql_con, reflect=True)
+# create (if not exist) pipeline table
+psql_pipeline = Table('pipelines', psql_meta,
+    Column('name', String, primary_key=True),
+    Column('content', JSONB),
+    extend_existing=True
+)
+psql_meta.create_all(psql_con)
 
 def list_jobs():
     return schedule.list_jobs()
-
 
 def remove_job(id):
     return schedule.delete_job(id)
@@ -68,22 +79,20 @@ def pipeline_init(tool, dataset):
                 "branch": branch_name,
                 "local_dir": local_dir
                }
-    try:
-        with open(f"{importdir}/pipelines.json", 'r') as pipeline_file:
-            pipelines = json.load(pipeline_file)
-    except:
-        pipelines = {"pipelines": {}}
-    with open(f"{importdir}/pipelines.json", 'w') as pipeline_file:
-        pipelines['pipelines'][tool] = pipeline
-        json.dump(pipelines, pipeline_file, indent=4)
+    
+    # psql pipeline store
+    _new_pipeline = psql_pipeline.insert().values(name=tool, content=pipeline)
+    psql_con.execute(_new_pipeline)
+    
     return pipeline
 
 ### Scheduling not supported yet
 def pipeline_run(name, cron):
-    # Read config
-    with open(f"{importdir}/pipelines.json", 'r') as pipeline_file:
-        pipelines = json.load(pipeline_file)
-    pipeline = pipelines['pipelines'][name]
+    # read config from psql pipeline store
+    _query = select(
+        [psql_pipeline.c.content]).where(psql_pipeline.c.content['tool'] == cast(name, JSONB)
+    )
+    pipeline = psql_con.execute(_query).fetchone()[0]
     local_dir = pipeline['local_dir']
     host_dir = hostdir+ "/" + name
     # Run the tool + Mount the branch folder
