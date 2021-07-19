3ccccdfc4e9ce08198deeb65b8ec318c5f10d173
Daniel Hass <hassdan1@students.zhaw.ch>
2021-07-19 12:31:37 +0000

    introduced workaround for git permission error

--- executor.py
+++ executor.py
@@ -2,14 +2,24 @@ from aiohttp import web
 import json
 import docker
 import json
+import subprocess
 
 
 DOCKER_SOCKET_PATH = '/var/run/docker.sock'
+OUTPUT_UID = 1000
 
 app = web.Application()
 routes = web.RouteTableDef()
 
 docker_client = docker.from_env()
+
+def chown_output(uid, dir):
+    # spawn a busybox with output repo mounted to adjust the file permissions
+    docker_client.containers.run("busybox:latest",
+                            volumes={dir: {'bind': '/usr/src/app/data'}},
+                            command=["/bin/sh", "-c", f"chown -R {uid}:{uid} /usr/src/app/data && sleep 1"],
+                            auto_remove=True
+                            )
 
 def pipeline_run(image, input_dir, output_dir, env, cmd, docker_socket):
     print(f"Running image {image} with input_dir {input_dir} and output_dir {output_dir}")
@@ -19,6 +29,7 @@ def pipeline_run(image, input_dir, output_dir, env, cmd, docker_socket):
         _volumes[DOCKER_SOCKET_PATH] = {'bind': DOCKER_SOCKET_PATH}
     if input_dir is not None:
         _volumes[input_dir] = {'bind': '/usr/src/app/input'}
+    # execute actual tool image with provided settings
     docker_client.containers.run(image,
                             volumes=_volumes,
                             network='host',
@@ -27,6 +38,8 @@ def pipeline_run(image, input_dir, output_dir, env, cmd, docker_socket):
                             auto_remove=True
                             )
     print("Done")
+    print("Correcting ownership of pipeline step output ...")
+    chown_output(OUTPUT_UID, output_dir)
     return {"image_used": image, "output_dir": output_dir}
 
 @routes.post('/run')
